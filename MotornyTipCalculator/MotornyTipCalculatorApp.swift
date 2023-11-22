53a30d8169064fbbe2d9ada020720b3a769d7855
Maksym Motorny <mmotorny@gmail.com>
2023-11-21 20:43:20 -0800

    Initial Commit

---
+++ MotornyTipCalculator/MotornyTipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotornyTipCalculatorApp.swift
+//  MotornyTipCalculator
+//
+//  Created by Maksym Motornyy on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MotornyTipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
